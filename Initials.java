1354f61298d7a1610068d7abb56263f4a2436d38
Willie Suggs <klbooker09@gmail.com>
2017-05-05 02:00:29 -0500

    Add files via upload

---
+++ Initials.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package initials;
+
+/**
+ *
+ * @author kendrabooker
+ */
+public class Initials {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("W     W  L       SSS\nW     W  L      S   S\nW     W  L      S\nW  W  W  L       SSS\nW W W W  L          S\nW W W W  L      S   S\nW     W  LLLLL   SSS");
+    }
+    
+}
